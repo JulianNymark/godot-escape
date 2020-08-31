9cdd997e085aad2751702640d8b0cb2de3be8f47
Julian Nymark <j@juliannymark.com>
2020-08-31 23:19:47 +0200

    rudimentary generation & instantiating of Terrain

--- Controller.gd
+++ Controller.gd
@@ -2,36 +2,44 @@ extends Node2D
 
 enum Cardinals {N, S, E, W}
 
-var SPEED = 100
+const CONSTANTS = preload("constants.gd")
+
+const SPEED = 100
+
 var animationPlayer = null
 var facing = Cardinals.S
+var velocity = Vector2(0, 0)
+var idx = Vector2(0, 0)
 
 func _ready():
 	animationPlayer = $AnimationPlayer
 
-func _process(delta):
-	var velocity = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	)
-	
-	handleAnimation(velocity)
-	
-	self.position += (velocity * SPEED * delta)
-
-func handleAnimation(velocity):
-	if (velocity.x > 0):
-		facing = Cardinals.E
-		animationPlayer.play("WalkEast");
-	elif (velocity.x < 0):
-		facing = Cardinals.W
-		animationPlayer.play("WalkWest");
-	elif (velocity.y > 0):
-		facing = Cardinals.S
-		animationPlayer.play("WalkSouth");
-	elif (velocity.y < 0):
+func _process(delta):	
+	velocity = Vector2(0, 0)
+	if (Input.is_action_just_pressed("ui_up")):
 		facing = Cardinals.N
+		velocity = Vector2(0, -1)
+	elif (Input.is_action_just_pressed("ui_down")):
+		facing = Cardinals.S
+		velocity = Vector2(0, 1)
+	elif (Input.is_action_just_pressed("ui_left")):
+		facing = Cardinals.W
+		velocity = Vector2(-1, 0)
+	elif (Input.is_action_just_pressed("ui_right")):
+		facing = Cardinals.E
+		velocity = Vector2(1, 0)
+	
+	handleAnimation(facing)
+	
+	idx += velocity
+	self.position = idx * CONSTANTS.GRID_SIZE
+
+func handleAnimation(direction):
+	if (direction == Cardinals.E):
+		animationPlayer.play("WalkEast");
+	elif (direction == Cardinals.W):
+		animationPlayer.play("WalkWest");
+	elif (direction == Cardinals.S):
+		animationPlayer.play("WalkSouth");
+	elif (direction == Cardinals.N):
 		animationPlayer.play("WalkNorth");
-		
-	
-	
---
+++ Map.gd
@@ -0,0 +1,50 @@
+extends YSort
+
+const Rock = preload("Terrain/Rock.tscn")
+const Grass = preload("Terrain/Grass.tscn")
+const NoTexture = preload("Terrain/NoTexture.tscn")
+
+const CONSTANTS = preload("constants.gd")
+const DIM = Vector2(10,10)
+
+var mapData = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	mapData = generateMapBlueprint(DIM)
+	instantiateTerrain(mapData)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# 2d array of strings that define which Terrain blocks to instantiate
+func generateMapBlueprint(dimensions):
+	var generated = []
+	var toAdd = null
+	for i in range(dimensions.y):
+		toAdd = []
+		for l in range(dimensions.x):
+			if randf() > 0.9:
+				toAdd.append("Rock")
+			else:
+				toAdd.append("Grass")
+		generated.append(toAdd)
+	
+	return generated
+
+func instantiateTerrain(terrainBlueprint):
+	
+	for y in range(len(terrainBlueprint)):
+		for x in range(len(terrainBlueprint[0])):
+			
+			var node = null
+			if terrainBlueprint[y][x] == "Rock":
+				node = Rock.instance()
+			elif terrainBlueprint[y][x] == "Grass":
+				node = Grass.instance()
+			else:
+				node = NoTexture.instance()
+			node.position = Vector2(x, y) * CONSTANTS.GRID_SIZE
+			self.add_child(node)
