79342755e42ca8ddc7dc52bcf6675af7bd3acf89
Julian Nymark <j@juliannymark.com>
2020-08-31 19:30:37 +0200

    add animation to player controller

--- Controller.gd
+++ Controller.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-var SPEED = 100;
+enum Cardinals {N, S, E, W}
+
+var SPEED = 100
+var animationPlayer = null
+var facing = Cardinals.S
 
 func _ready():
-	pass
+	animationPlayer = $AnimationPlayer
 
 func _process(delta):
 	var velocity = Vector2(
@@ -11,4 +15,23 @@ func _process(delta):
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
 	
+	handleAnimation(velocity)
+	
 	self.position += (velocity * SPEED * delta)
+
+func handleAnimation(velocity):
+	if (velocity.x > 0):
+		facing = Cardinals.E
+		animationPlayer.play("WalkEast");
+	elif (velocity.x < 0):
+		facing = Cardinals.W
+		animationPlayer.play("WalkWest");
+	elif (velocity.y > 0):
+		facing = Cardinals.S
+		animationPlayer.play("WalkSouth");
+	elif (velocity.y < 0):
+		facing = Cardinals.N
+		animationPlayer.play("WalkNorth");
+		
+	
+	
